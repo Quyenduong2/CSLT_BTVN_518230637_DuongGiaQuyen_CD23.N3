d04de89b74c0ba3ea69c4642157dd58e958da77a
Quyen <duellink1210@gmail.com>
2023-12-01 21:44:31 +0700

    bai tap CSLT

---
+++ src/BTVN.java
@@ -0,0 +1,244 @@
+import java.util.Scanner;
+
+public class BTVN {
+    public static void main(String[] args) {
+//        VD/ trang 20
+//        System.out.print(“Hello hotboy and hotgirl !");
+
+//        VD/ trang 21
+//        System.out.print("Hello hotboy and hotgirl !");
+//        System.out.print("How are you ?");
+
+//        VD/ trang 22
+//        System.out.println("Hello hotboy and hotgirl !");
+//        System.out.print("How are you ?");
+
+//        VD / trang 23
+//        int tuoi = 18;
+//        System.out.print("Ban may tuoi ?");
+//        System.out.print(tuoi);
+
+//        VD / trang 24
+//        int tuoi = 18;
+//        System.out.println("Ban may tuoi ?");
+//        System.out.print("Em chua " + tuoi);
+
+
+//        VD / trang 26
+//        System.out.print("Em thich gi ?\n");
+//        System.out.print("\tAn\n\tUong\n");
+//        System.out.print("\tNgu");
+
+
+//        BT  / trang 27
+//        System.out.println("HelloHello \nHello\t\tHello\n\nHello\t\t\t\t\tHello");
+
+//        BT The Works of Wolfgang / trang 28
+//        System.out.print("The works of Wolfgang\ninclude ");
+//        System.out.print("the following");
+//        System.out.print("\nThe Turkish March ");
+//        System.out.print("and Symphony No. 40 ");
+//        System.out.println("in G minor.");
+
+//        ket qua xuat
+//        The works of Wolfgang
+//        include the following
+//        The Turkish March and Symphony No. 40 in G minor.
+
+
+
+//        BT / trang 29
+//        dayOfWeek đúng
+//        3dGraph Sai vì chữ đầu k phải chữ cái
+//        june1997 đúng
+//        mixture#3 sai vì kí tự # k đc đặt tên biến chỉ đc $
+//        week day sai vì có khoảng trắng
+
+
+//        BT / trang 30/ ket qua xuat
+//        int little;
+//        int big;
+//        little = 2;
+//        big = 2000;
+//        System.out.println("The little number is " + little);
+//        System.out.println("The big number is " + big);
+
+//        The little number is 2
+//        The big number is 2000
+
+//        1) Viết chương trình nhập xuất ra màn hình thông tin của mỗi sinh viên theo mẫu sau:
+//        Ho ten:
+//        Ma so sinh vien:
+//        Lop:
+//        So dien thoai: Gioi tinh:
+
+//        System.out.println("Ho Ten: ");
+//        System.out.println("Ma so sinh vien:");
+//        System.out.println("Lop: ");
+//        System.out.println("So dien thoai:\t\t\t\t\tGioi tinh:");
+
+//        2) Viết chương trình xuất ra màn hình bài thơ sau:
+//        System.out.println("XUÂN VỀ");
+//        System.out.println("Từng đàn con trẻ chạy xun xoe");
+//        System.out.println("Mưa lạnh, trời quang, nắng mới hoe");
+//        System.out.println("Lá nõn nhành non, ai tráng bạc?");
+//        System.out.println("Gió về từng tận, gió bay đi...\n");
+//        System.out.println("\tThong thả dân gian nghỉ việc đồng");
+//        System.out.println("\tLúa thì con gái mượt như nhung");
+//        System.out.println("\tĐầy vươn hoa bưởi, hoa cam rụng");
+//        System.out.println("\tNgào ngạt hương bay, bướm vẽ vòng\n");
+//        System.out.println(" Trên đường cát mịn, một đôi cô");
+//        System.out.println(" Yếm đỏ, khăn thâm, trẩy hội chùa");
+//        System.out.println(" Gậy trúc dắt bà già tóc bạc");
+//        System.out.println(" Tay lần tràng hạt, miệng nam mô.");
+
+//        3) Viết chương trình xuất ra màn hình theo mẫu sau:
+//        System.out.println("/*****************************************/");
+//        System.out.println("/******* HUONG DAN CHEP TAP TIN *******/");
+//        System.out.println("/=>B1. Vao thu muc “C:\\TUYENTAP\\thotinh.txt” /");
+//        System.out.println("/=>B2. Click chuot phai vao tap tin thotinh.txt /");
+//        System.out.println("/=>B3. Chon copy tu menu tat ");
+//        System.out.println("/=>B4. Chon vi tri can luu, click phai chon paste /");
+//        System.out.println("/*****************************************/");
+
+//        4) Viết chương trình nhập vào 2 số nguyên dương a và b, cho biết kết quả chia lấy phần
+//        nguyên và phần dư của a với b.
+
+//        double a ,b;
+//
+//        Scanner keyboard = new Scanner(System.in);
+//
+//        System.out.print("Nhap a: ");
+//        a = keyboard.nextDouble();
+//        System.out.print("Nhap b: ");
+//        b = keyboard.nextDouble();
+//
+//        System.out.println("KQ Chia lay phan nguyen: " + (a/b));
+//        System.out.println("KQ Chia lay phan du: " + (a%b));
+
+//        5) Viết chương trình nhập một số nguyên dương N có 2 chữ số từ bàn phím, xuất ra màn
+//        hình tổng các chữ số của N
+
+//        int a,S;
+//
+//        Scanner keyboard = new Scanner(System.in);
+//
+//        System.out.print("Nhap so nguyen duong co 2 chu so: ");
+//        a = keyboard.nextInt();
+//
+//        S = (a/10) + ( a%10);
+//
+//        System.out.println("S = " + S);
+
+//        6) Viết chương trình cho phép nhập vào một số đo nhiệt độ theo độ Fahrenheit và xuất
+//        ra nhiệt độ tương đương của nó theo độ Celcius, sử dụng công thức chuyển đổi:
+
+//        double C,F ;
+//
+//        Scanner keyboard = new Scanner(System.in);
+//        System.out.print("Nhap do F: ");
+//        F = keyboard.nextDouble();
+//
+//        C = (5/9f) * (F - 32);
+//
+//        System.out.println("Nhiet do (C) = "+ C);
+
+//        8) Viết chương trình cho phép nhập vào thời gian của một công việc nào đó tính bằng
+//        giây. Hãy chuyển đổi và in ra màn hình thời gian trên dưới dạng bao nhiêu giờ, bao nhiêu
+//        phút, bao nhiêu giây
+
+//        int n,h,m,s;
+//        Scanner keyboard = new Scanner(System.in);
+//
+//        System.out.print("Nhap thoi gian cong viec (giay): ");
+//        n = keyboard.nextInt();
+
+//        h = n/3600;
+//        m = (n%3600)/60;
+//        s = n%60;
+
+//        System.out.println(h +" Gio " + m +" phut " + s + " giay ");
+
+//        9) Viết chương trình nhập vào 2 số nguyên a, b. Tính tổng, hiệu, tích, thương của 2 số
+//        trên và in kết quả ra màn hình.
+
+//        int a,b;
+//
+//        Scanner keyboard = new Scanner(System.in);
+//
+//        System.out.print("Nhap a: ");
+//        a = keyboard.nextInt();
+//        System.out.print("Nhap b: ");
+//        b = keyboard.nextInt();
+//
+//        System.out.println("S = " + (a + b));
+//        System.out.println("Hieu = " + (a - b));
+//        System.out.println("P = " + (a * b));
+//        System.out.println("Thuong = " + ((float)a / b));
+
+//        10) Viết chương trình nhập vào 4 số nguyên a, b, c, d. Tính giá trị trung bình cộng của 4
+//        số trên và in kết quả ra màn hình.
+
+//        double a,b,c,d,sum;
+//        Scanner keyboard = new Scanner(System.in);
+//
+//        System.out.print("Nhap a: ");
+//        a = keyboard.nextInt();
+//        System.out.print("Nhap b: ");
+//        b = keyboard.nextInt();
+//        System.out.print("Nhap c: ");
+//        c = keyboard.nextInt();
+//        System.out.print("Nhap d: ");
+//        d = keyboard.nextInt();
+//
+//        sum = (a + b + c + d) / 4;
+//
+//        System.out.println("Trung binh cong = " + sum);
+
+//        11) Viết chương trình tính diện tích và chu vi các hình: tam giác, hình vuông, hình chữ nhật
+//        và hình tròn với những thông tin cần được nhập từ bàn phím.
+
+//        double a,b,c,r,h,d;
+//
+//        Scanner keyboard = new Scanner(System.in);
+//
+//        System.out.print("nhap a: ");
+//        a = keyboard.nextDouble();
+//        System.out.print("nhap b: ");
+//        b = keyboard.nextDouble();
+//        System.out.print("nhap c: ");
+//        c = keyboard.nextDouble();
+//        System.out.print("nhap chieu cao h: ");
+//        h = keyboard.nextDouble();
+//        System.out.print("nhap ban kinh r: ");
+//        r = keyboard.nextDouble();
+//        System.out.print("nhap duong kinh d: ");
+//        d = keyboard.nextDouble();
+//
+//        System.out.println("S hinh vuong la: " + (a*a));
+//        System.out.println("P hinh vuong la: " + (4*a));
+//        System.out.println("S hinh tam giac ( can ) la: " + 1/2*(a*h));
+//        System.out.println("P hinh tam giac ( can ) la: " + (2*a + c));
+//        System.out.println("S hcn la: " + (a*b));
+//        System.out.println("P hcn la: " + (2*(a + b)));
+//        System.out.println("S hinh tron la: " + (Math.PI * Math.pow(d/2,2) ));
+//        System.out.println("P hinh tron la: " + ((r*2)*Math.PI));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
